c0ce4aefa3cce80ad7a64ed309d3513767401f1a
guvensahin <guvensahin@outlook.com>
2020-02-11 18:26:43 +0300

    model düzenlemesi

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
 
 
 <div class="modal fade" tabindex="-1" role="dialog">
-  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+  <div class="modal-dialog modal-lg" role="document">
     
     <div class="modal-content">
       <div class="modal-header">
